a4a237f38343d32ca24fd799c38c8a52ae9b81b6
Skplunkerin <topher@smoothblack6.com>
2023-01-16 05:33:48 -0700

    Create Faucet.sol Smart Contract (#1)
    > **NOTE:** unless the testnet is setup to pull ETH out of nowhere, this
    > smart contract being a "Faucet" shouldn't work since the contract has
    > a balance of 0.
    
    * Add new `Faucet.sol` contract
    
    * Update `README.md` with:
      - Add project setup steps
      - Add link to deployed testnet smart contract
    
    * Update `package.json`:
      - Add new `compile` script
      - Add author
      - Install `@nomicfoundation/hardhat-chai-matchers`
      - Uninstall `@nomiclabs/hardhat-waffle` and `ethereum-waffle`
        (at Hardhat cli suggestion of migrating off of those to use
        `hardhat-chai-matchers` instead
    
    * Update `deploy.js` script:
      - Replace init code with `Faucet.sol` deployment needs

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,33 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+require("dotenv").config();
+const { ethers } = require("ethers");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  let artifacts = await hre.artifacts.readArtifact("Faucet");
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.TESTNET_ALCHEMY_RPC_URL
   );
+  let wallet = new ethers.Wallet(
+    process.env.TESTNET_WALLET_PRIVATE_KEY,
+    provider
+  );
+
+  // Create an instance of a Faucet Factory
+  let factory = new ethers.ContractFactory(
+    artifacts.abi,
+    artifacts.bytecode,
+    wallet
+  );
+
+  let faucet = await factory.deploy();
+
+  console.log("Faucet address:", faucet.address);
+
+  await faucet.deployed();
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
