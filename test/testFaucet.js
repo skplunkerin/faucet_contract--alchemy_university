ec48f80edbd9ccafa117c1dc98ec19b8d7440910
skplunkerin <topher@smoothblack6.com>
2023-01-17 14:37:40 -0700

    * Add test for only owners can call `destroyFaucet()`

--- test/testFaucet.js
+++ test/testFaucet.js
@@ -45,6 +45,7 @@ describe("Faucet", function () {
     const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
     expect(await faucet.owner()).to.equal(owner.address);
   });
+
   it("withdraw() should send only .1 ETH", async function () {
     const { faucet, owner, otherAccount, maxWithdrawAmount } =
       await loadFixture(deployContractAndSetVariables);
@@ -53,36 +54,12 @@ describe("Faucet", function () {
     await expect(faucet.connect(otherAccount).withdraw(withdrawAmount)).to.be
       .reverted;
 
-    // TODO: allow the correct withdraw amount
-    //
-    // Attempt 1:
-    // This isn't working... I want to get the contract balance before/after but
-    // it isn't changing (something might be wrong with the contract function?
-    // or I'm not doing something right here). [topher]
-    //
-    // const contractBalance = await ethers.provider.getBalance(faucet.address);
-    // await expect(faucet.connect(otherAccount).withdraw(maxWithdrawAmount));
-    // const newContractBalance = await ethers.provider.getBalance(faucet.address);
-    // // NOTE: the balance isn't changing like it's supposed to.
-    // expect(contractBalance).to.not.equal(newContractBalance);
-    //
-    // Attempt 2:
-    // this also doesn't work (there's probably something wrong with
-    // `Faucet.sol`):
-    //
-    // await expect(
-    //   faucet.connect(otherAccount).withdraw(maxWithdrawAmount)
-    // ).to.changeEtherBalances(
-    //   [faucet, otherAccount],
-    //   [-maxWithdrawAmount, maxWithdrawAmount]
-    // );
-    //
-    // Attempt 3 works:
     // TODO: how to make the BigNumber negative? none of the below works:
     //   maxWithdrawAmount * -1
     //   maxWithdrawAmount.times(-1)
     //   BigNumber(maxWithdrawAmount).times(-1)
     const negativeMaxWithdrawAmount = ethers.utils.parseUnits("-.1", "ether");
+    // to just check the balance changing to the contract or address:
     // await expect(
     //   faucet.connect(otherAccount).withdraw(maxWithdrawAmount)
     // ).to.changeEtherBalance(faucet, negativeMaxWithdrawAmount);
@@ -93,19 +70,31 @@ describe("Faucet", function () {
       [negativeMaxWithdrawAmount, maxWithdrawAmount]
     );
   });
+
+  it("should restrict destroyFaucet() to the owner via the onlyOwner modifier", async function () {
+    const { faucet, owner, otherAccount } = await loadFixture(
+      deployContractAndSetVariables
+    );
+
+    // another address can't call destroyFaucet()
+    await expect(faucet.connect(otherAccount).destroyFaucet()).to.be.reverted;
+
+    // the owner can call destroyFaucet()
+    await expect(faucet.connect(owner).destroyFaucet());
+    let bytecode = await ethers.provider.getCode(faucet.address);
+    expect(bytecode).equal("0x");
+  });
+
   // TODO: Test the following:
   // it("should restrict withdrawAll() to the owner via the onlyOwner modifier", async function () {
   //   const { faucet, owner, otherAccount } = await loadFixture(
   //     deployContractAndSetVariables
   //   );
   // });
-  // it("should restrict destroyFaucet() to the owner via the onlyOwner modifier", async function () {
+  // it("destroyFaucet() should send all funds to the owner", async function () {
   //   const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
   // });
   // it("withdrawAll() should send all funds to the owner", async function () {
-  //   const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
-  // });
-  // it("destroyFaucet() should send all funds to the owner", async function () {
   //   const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
   // });
 });
